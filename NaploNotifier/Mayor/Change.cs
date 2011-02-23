b2352984948c4273d1be9ec7182b448696a33750
Mate Solymosi <mate@solymosi.eu>
2011-02-23 18:05:03 +0100

    Finished refactoring Change.cs

--- NaploNotifier/Mayor/Change.cs
+++ NaploNotifier/Mayor/Change.cs
@@ -8,59 +8,37 @@ namespace NaploNotifier
     [Serializable()]
     public class Change
     {
-        public Note OldNote;
-        public Note NewNote;
+        public Note Old;
+        public Note New;
         public DateTime Date;
+        public string Title
+        {
+            get
+            {
+                switch (this.Type)
+                {
+                    case ChangeType.Added: return "Új " + this.New.Subject.Name + " jegyet kaptál";
+                    case ChangeType.Deleted: return "Törölve lett egy " + this.New.Subject.Name + " jegyed";
+                    case ChangeType.Modified: return "Megváltozott egy " + this.New.Subject.Name + " jegyed";
+                }
+                throw new ArgumentException();
+            }
+        }
         public ChangeType Type
         {
             get
             {
-                if (OldNote == null) { return ChangeType.Added; }
-                if (NewNote == null) { return ChangeType.Deleted; }
+                if (Old == null) { return ChangeType.Added; }
+                if (New == null) { return ChangeType.Deleted; }
                 return ChangeType.Modified;
             }
         }
 
-        public Change(Note old, Note newnote)
+        public Change(Note Old, Note New)
         {
-            this.OldNote = old;
-            this.NewNote = newnote;
+            this.Old = Old;
+            this.New = New;
             this.Date = DateTime.Now;
-        }
-
-        public override string ToString()
-        {
-            switch (this.Type)
-            {
-                case ChangeType.Added: return this.Date.ToString() + "\r\nÚJ JEGYET KAPTÁL:\r\n" + this.NewNote.BalloonDescription();
-                case ChangeType.Deleted: return this.Date.ToString() + "\r\nTÖRÖLVE LETT EGY JEGYED:\r\n" + this.OldNote.BalloonDescription();
-                case ChangeType.Modified: return this.Date.ToString() + "\r\nMEGVÁLTOZOTT EGY JEGYED:\r\n" + this.OldNote.BalloonDescription() + "\r\nÚJ JEGY:\r\n" + this.NewNote.BalloonDescription();
-            }
-            return "";
-        }
-
-        public string Text()
-        {
-            switch (this.Type)
-            {
-                case ChangeType.Added: return this.NewNote.BalloonDescription();
-                case ChangeType.Deleted: return this.OldNote.BalloonDescription();
-                case ChangeType.Modified: return this.OldNote.BalloonDescription() + "\r\n==>\r\n" + this.NewNote.BalloonDescription();
-            }
-            return "";
-        }
-
-
-
-        public string Title()
-        {
-            switch (this.Type)
-            {
-                case ChangeType.Added: return "Új " + this.NewNote.Subject.Name + " jegyet kaptál";
-                case ChangeType.Deleted: return "Törölve lett egy " + this.OldNote.Subject.Name + " jegyed";
-                case ChangeType.Modified: return "Megváltozott egy " + this.NewNote.Subject.Name + " jegyed";
-            }
-            return "";
         }
     }
 
